09c2bb83eb60da569ca57771890ee34908ec2093
simlpse <128969611+simlpse@users.noreply.github.com>
2024-04-06 13:48:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-from flask import Flask, request
-from doPost import doPost
+from flask import Flask, request, jsonify
+import json
+import requests
 
 app = Flask(__name__)
 
-@app.route('/api/doPost', methods=['POST'])
-def handle_doPost():
-    return doPost(request.data)
+@app.route('/doPost', methods=['POST'])
+def doPost():
+    # Récupérer les données envoyées par le raccourci
+    request_data = request.get_json()
+    link = request_data['link']
+    header = request_data['header']
+    content = request_data['content']
+
+    # Effectuer la requête POST
+    response = requests.post(link, headers=header, data=content)
+
+    # Récupérer les en-têtes et le contenu de la réponse
+    response_headers = response.headers
+    response_body = response.text
+
+    # Renvoyer les en-têtes et le contenu de la réponse au raccourci
+    responseData = {
+        'headers': dict(response_headers),
+        'body': response_body
+    }
+
+    return jsonify(responseData)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
